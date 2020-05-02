a272b3f99f932054305098f7854e120f16568057
Bruno Prieto Reis <bruno.p.reis@gmail.com>
2020-05-02 01:29:24 +0000

    testing sw

--- next.config.js
+++ next.config.js
@@ -1,35 +1,36 @@
-const withOffline = require('next-offline')
+// const withOffline = require('next-offline')
+module.exports = {}
 
-module.exports = withOffline({
-	target: process.env.NEXT_TARGET || 'serverless',
-	workboxOpts: {
-		swDest: 'static/service-worker.js',
-		skipWaiting: true,
-		runtimeCaching: [
-			{
-				urlPattern: /[.](png|jpg|ico|css)/,
-				handler: 'StaleWhileRevalidate',
-				options: {
-					cacheName: 'assets-cache',
-					cacheableResponse: {
-						statuses: [0, 200]
-					}
-				}
-			},
-			{
-				urlPattern: /^https:\/\/code\.getmdl\.io.*/,
-				handler: 'StaleWhileRevalidate',
-				options: {
-					cacheName: 'lib-cache'
-				}
-			},
-			{
-				urlPattern: /^http.*/,
-				handler: 'NetworkFirst',
-				options: {
-					cacheName: 'http-cache'
-				}
-			}
-		]
-	}
-})
+// module.exports = withOffline({
+// 	target: process.env.NEXT_TARGET || 'serverless',
+// 	workboxOpts: {
+// 		swDest: 'static/service-worker.js',
+// 		skipWaiting: true,
+// 		runtimeCaching: [
+// 			{
+// 				urlPattern: /[.](png|jpg|ico|css)/,
+// 				handler: 'StaleWhileRevalidate',
+// 				options: {
+// 					cacheName: 'assets-cache',
+// 					cacheableResponse: {
+// 						statuses: [0, 200]
+// 					}
+// 				}
+// 			},
+// 			{
+// 				urlPattern: /^https:\/\/code\.getmdl\.io.*/,
+// 				handler: 'StaleWhileRevalidate',
+// 				options: {
+// 					cacheName: 'lib-cache'
+// 				}
+// 			},
+// 			{
+// 				urlPattern: /^http.*/,
+// 				handler: 'NetworkFirst',
+// 				options: {
+// 					cacheName: 'http-cache'
+// 				}
+// 			}
+// 		]
+// 	}
+// })
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,37 @@
+
 import 'isomorphic-fetch'
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Timeline } from '../components/Timeline'
 
-const Index = ({ stars }) => (
-	<>
 
-	<Timeline />
-	<style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
 
-        * {
-          box-sizing: border-box;
-        }
-	`}</style>
-	</>
-)
+const Index = ({ stars }) => {
+	useEffect(
+		() => {
+			navigator.serviceWorker.register('/sw.js')
+				.then(reg => console.log('SW registered!', reg))
+				.catch(err => console.log('Boo!', err));
+		}
+	)
+	return (
+		<>
+		<Timeline />
+		<style jsx global>{`
+					html,
+					body {
+						padding: 0;
+						margin: 0;
+						font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+							Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+							sans-serif;
+					}
+
+					* {
+						box-sizing: border-box;
+					}
+		`}</style>
+		</>
+	)
+}
 
 export default Index
