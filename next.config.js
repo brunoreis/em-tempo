009c4d96034be9515a6a33861f7532387604586a
Bruno Prieto Reis <bruno.p.reis@gmail.com>
2020-05-29 00:30:18 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,40 +1,10 @@
 // const withOffline = require('next-offline')
 const withImages = require('next-images')
 
-module.exports = withImages()
+module.exports = withImages({
+  assetPrefix: process.env.BASE_PATH || '',
+  publicRuntimeConfig: {
+    basePath: process.env.BASE_PATH || '',
+  },
+})
 
-// module.exports = withOffline({
-// 	// target: process.env.NEXT_TARGET || 'serverless',
-// 	target: 'serverless',
-// 	// generateInDevMode: true,
-// 	workboxOpts: {
-// 		swDest: 'static/service-worker.js',
-// 		// 	skipWaiting: true,
-// 		// 	runtimeCaching: [
-// 		// 		{
-// 		// 			urlPattern: /[.](png|jpg|ico|css)/,
-// 		// 			handler: 'StaleWhileRevalidate',
-// 		// 			options: {
-// 		// 				cacheName: 'assets-cache',
-// 		// 				cacheableResponse: {
-// 		// 					statuses: [0, 200],
-// 		// 				},
-// 		// 			},
-// 		// 		},
-// 		// 		{
-// 		// 			urlPattern: /^https:\/\/code\.getmdl\.io.*/,
-// 		// 			handler: 'StaleWhileRevalidate',
-// 		// 			options: {
-// 		// 				cacheName: 'lib-cache',
-// 		// 			},
-// 		// 		},
-// 		// 		{
-// 		// 			urlPattern: /^http.*/,
-// 		// 			handler: 'NetworkFirst',
-// 		// 			options: {
-// 		// 				cacheName: 'http-cache',
-// 		// 			},
-// 		// 		},
-// 		// 	],
-// 	},
-// })
